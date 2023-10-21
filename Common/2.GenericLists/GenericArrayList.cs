2f1f5008503957bd6b513c9098c02dbc33fd77b3
bsagarzazu <sagartertzio25@gmail.com>
2023-10-21 14:14:33 +0200

    GenericList y GenericArrayList

--- Common/2.GenericLists/GenericArrayList.cs
+++ Common/2.GenericLists/GenericArrayList.cs
@@ -9,6 +9,7 @@ namespace Common
         public GenericArrayList(int n)
         {
             //TODO #1: initialize Values with an array of size n
+            Values = new T[n];
         }
         public string AsString()
         {
@@ -26,30 +27,56 @@ namespace Common
         public void Add(T value)
         {
             //TODO #2: add a new element to the end of the list
+            if (NumElements < Values.Length)
+            {
+                Values[NumElements] = value;
+            }
+            else
+            {
+                T[] newValues = new T[Values.Length + 1];
+                for (int i = 0; i < Values.Length; i++)
+                {
+                    newValues[i] = Values[i];
+                }
+                newValues[Values.Length] = value;
+                Values = newValues;
+            }
+            NumElements++;
         }
 
         public T Get(int index)
         {
             //TODO #3: return the element on the index-th position. YOU MUST USE GetNode(int). Return the default value for object class T if the position is out of bounds
-            
-            return default(T);
+            if (index < Values.Length)
+            {
+                return Values[index];
+            }
+            else
+            {
+                return default(T);
+            }
         }
 
         public int Count()
         {
             //TODO #4: return the number of elements on the list
-
-            return 0;
+            return NumElements;
         }
 
         public void Remove(int index)
         {
             //TODO #5: remove the element on the index-th position. Do nothing if position is out of bounds
+            for (int i = index; i < NumElements; i++)
+            {
+                Values[i] = Values[i + 1];
+            }
+            NumElements--;
         }
 
         public void Clear()
         {
             //TODO #6: remove all the elements on the list
+            NumElements = 0;
         }
     }
 }
--- Common/2.GenericLists/GenericList.cs
+++ Common/2.GenericLists/GenericList.cs
@@ -18,7 +18,9 @@ public class GenericListNode<T>
 
 public class GenericList<T> : IGenericList<T>
 {
-    GenericListNode<T> First = null;
+    private GenericListNode<T> First = null;
+    private GenericListNode<T> Last = null;
+    private int numElements = 0;
 
     public string AsString()
     {
@@ -38,36 +40,81 @@ public class GenericList<T> : IGenericList<T>
     public void Add(T value)
     {
         //TODO #1: add a new element to the end of the list
+        if (First == null)
+        {
+            First = new GenericListNode<T>(value);
+            Last = First;
+        }
+        else
+        {
+            Last.Next = new GenericListNode<T>(value);
+            Last = Last.Next;
+        }
+        numElements++;
     }
 
     public GenericListNode<T> FindNode(int index)
     {
         //TODO #2: Return the element in position 'index'
-
+        int currentPos = 0;
+        GenericListNode<T> currentNode = First;
+        while (currentPos < index && currentNode.Next != null)
+        {
+            currentNode = currentNode.Next;
+            currentPos++;
+        }
+        if (currentPos == index)
+        {
+            return currentNode;
+        }
         return null;
     }
 
     public T Get(int index)
     {
         //TODO #3: return the element on the index-th position. YOU MUST USE GetNode(int). Return the default value for object class T if the position is out of bounds
-
-        return default(T);
+        if (FindNode(index) != null)
+        {
+            return FindNode(index).Value;
+        }
+        else
+        {
+            return default(T);
+        }
     }
     public int Count()
     {
         //TODO #4: return the number of elements on the list
-
-        return 0;
+        return numElements;
     }
 
 
     public void Remove(int index)
     {
         //TODO #5: remove the element on the index-th position. Do nothing if position is out of bounds
+        GenericListNode<T> node = FindNode(index);
+        if (node == First)
+        {
+            First = First.Next;
+            numElements--;
+        }
+        else if (node == Last)
+        {
+            Last = FindNode(index - 1);
+            numElements--;
+        }
+        else if (node != null)
+        {
+            FindNode(index - 1).Next = FindNode(index + 1);
+            numElements--;
+        }
     }
 
     public void Clear()
     {
         //TODO #6: remove all the elements on the list
+        First = null;
+        Last = null;
+        numElements = 0;
     }
 }
